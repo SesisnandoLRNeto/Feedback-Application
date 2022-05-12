18963f8d7e1fc4b2c38f1ef8d798065c37b573da
SesisnandoLRNeto <nandolrneto@gmail.com>
2022-05-11 21:06:27 -0400

    Configure the port to run in the railway app platform

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,6 +9,6 @@ app.use(express.json());
 
 app.use(routes);
 
-app.listen(3333, () => {
-  console.log('listen port 3333');
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running in the port ${process.env.PORT}`);
 });
